17f238ac01955b5517d294064b2015b3d8b13a90
Gilles Crofils <gilles.crofils@gmail.com>
2009-07-12 00:48:52 +0200

    add config.gem shoulda

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "RedCloth", :source => "http://code.whytheluckystiff.net", :version => '>=4.2.1'
   config.gem "settingslogic"
+  config.gem "shoulda"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
