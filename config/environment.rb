e2329573722f24f9659f21571300f5ea53d0e17a
Gilles Crofils <gilles.crofils@gmail.com>
2009-07-12 00:59:42 +0200

    add config.gem shoulda

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem "RedCloth", :source => "http://code.whytheluckystiff.net", :version => '>=4.2.1'
   config.gem "settingslogic"
-  config.gem "shoulda"
+  config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
